2564024373b21b07cc9f1a8f9fb51e03c84f7c5c
Tong JIN <72709421+TongJIN-77@users.noreply.github.com>
2020-10-24 14:54:08 +0200

    Add files via upload
    initial commit

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var model_mobilenet;
+var video;
+var loaded;
+
+function setup() {
+  createCanvas(400, 300);
+  mobilenet.load().then(modelLoaded);
+  video = createCapture(VIDEO);
+  video.size(400, 300);
+  //video.hide();
+  createButton("Take a picture").mousePressed(btnClicked);
+  
+}
+
+function classifyDone(res) {
+  print(res);
+  if (res[0].className == "cellular telephone, cellular phone, cellphone, cell, mobile phone")
+  {
+    alert("There is a mobile phone in the picture");
+  } else if (res[0].className == "water bottle")
+  {
+     alert("There is a water bottle in the picture");
+  }
+  else if (res[0].className == "coffee mug")
+    {
+       alert("There is a coffee mug in the picture");
+    }
+  
+  else{
+      alert("None of the three objects in the picture");
+  }
+  
+}
+
+function modelLoaded(net) {
+  model_mobilenet = net;
+  loaded = true;
+  print("Model loaded");
+}
+
+function btnClicked() {
+  image(video, 0, 0, 400, 300);
+  if (loaded == true) {
+    model_mobilenet.classify(video.elt).then(classifyDone);
+  }
+}
